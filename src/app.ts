c5bd7436ef5baee01eaebbbc3a31038e1b907baa
Dovydas Jarukaitis <dovjar@gmail.com>
2020-04-04 13:01:25 +0300

    dual boot, read from config.json or from process.env

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { MessageHandler } from './handler';
 import Discord = require('discord.js');
 import mongoose from 'mongoose';
-
+const CONFIG = require('./../config.json') || {DISCORD_API_KEY:process.env.DISCORD_API_KEY, MONGODB_URL:process.env.MONGODB_URL};
 
 const BOT = new Discord.Client();
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(CONFIG.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).catch((e)=> console.error(e));
@@ -17,7 +17,7 @@ db.once('open', () => {
     console.info('Mongo Connected');
 });
 
-BOT.login(process.env.DISCORD_API_KEY)
+BOT.login(CONFIG.DISCORD_API_KEY)
    .then(() => console.log('Successfully logged as', BOT.user.username))
    .catch((error) => console.log(error));
 
