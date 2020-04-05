b9389620caa034a970b177613544d5f05c865c2e
Dovydas Jarukaitis <dovjar@gmail.com>
2020-04-05 18:44:20 +0300

    fix p c axe =>xe

--- src/messageParsers/char/combatSkill.ts
+++ src/messageParsers/char/combatSkill.ts
@@ -8,7 +8,7 @@ import { CombatModeEnum } from '../../models/char';
 export const commandParser:IMessageParser = {
   priority:0,
   async createCommand(cut:string):Promise<ICommand[]>{
-    const args = cut.match(/p\s+(combat|c)(\s+(add|a)*(remove|r)*\s*([a-z0-9._]+)\s*((\d+)?\s*(a=(\S+))*\s*(range|melee|defense)*)*)*/i);
+    const args = cut.match(/p\s+(combat|c)(\s+(add)?(remove)?\s*([a-z0-9._]+)\s*((\d+)?\s*(a=(\S+))*\s*(range|melee|defense)*)*)*/i);
     if (args && (args[7] || args[3] || args[9] || args[10]) && (!args[4]))
       return [new CharAddCombatSkillCommand(args[5], parseInt(args[7], 10), args[9], CombatModeEnum[args[10]]),
               new CharPrintCommand(PrintCommandsEnum.combat)];
