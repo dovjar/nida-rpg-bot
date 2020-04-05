b799cd84c618629679f49751efab038429145333
Dovydas Jarukaitis <dovjar@gmail.com>
2020-04-05 11:31:57 +0300

    printCommandHandler

--- src/handlers/npc/printCommandHandler.ts
+++ src/handlers/npc/printCommandHandler.ts
@@ -10,7 +10,7 @@ export const commandHandler:ICommandHandler = {
       return null;
 
     const maxSkills = context.globalContext.npc.map((npc)=>{ return npc.skills.length }).reduce((a,b)=>{return Math.max(a,b)}) || 0;
-    const header=['npc',...new Array(maxSkills).map(t=>'')];
+    const header=['npc',...new Array(maxSkills).map(()=>{return 'skill'})];
     const rows = context.globalContext.npc.map((npc)=>{ return [npc.name, ...npc.skills.map(t=>`${t.name}=${t.lvl}`)]});
     const wt = new WordTable(header, rows);
     return new CommandResult(`**NPC**\n\`\`\`asciidoc\n${wt.string()}\n\`\`\``);
